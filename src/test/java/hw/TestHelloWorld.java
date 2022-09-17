4df85b518f053353b3f49abd7b1a333b0a1c03d2
Konstantin Läufer <laufer@cs.luc.edu>
2022-09-16 21:08:19 -0500

    auto-reformatted Java sources
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/test/java/hw/TestHelloWorld.java
+++ src/test/java/hw/TestHelloWorld.java
@@ -15,20 +15,19 @@ public class TestHelloWorld {
 
   @Property
   boolean absoluteValueOfAllNumbersIsPositive(@ForAll final int anInteger) {
-	return Math.abs(anInteger) >= 0;
+    return Math.abs(anInteger) >= 0;
   }
 
   @Property
-	void lengthOfConcatenatedStringIsGreaterThanLengthOfEach(
-		@ForAll final String string1, @ForAll final String string2
-	) {
-		final var conc = string1 + string2;
-		assertTrue(conc.length() > string1.length());
-		assertTrue(conc.length() > string2.length());
-	}  
+  void lengthOfConcatenatedStringIsGreaterThanLengthOfEach(
+      @ForAll final String string1, @ForAll final String string2) {
+    final var conc = string1 + string2;
+    assertTrue(conc.length() > string1.length());
+    assertTrue(conc.length() > string2.length());
+  }
 
   @Example
-  void squareRootOf16is4() { 
+  void squareRootOf16is4() {
     assertEquals(4.0, Math.sqrt(16), 0.01);
   }
 
@@ -39,31 +38,31 @@ public class TestHelloWorld {
 
   @Example
   void testHello1() {
-	assertEquals("hello", fixture.echo("hello"));
+    assertEquals("hello", fixture.echo("hello"));
   }
 
   @Example
   void testHello2() {
-	assertEquals("", fixture.echo(""));
+    assertEquals("", fixture.echo(""));
   }
 
   @Property
   boolean echoReturnsItsArgument(@ForAll final String s) {
-	return fixture.echo(s).equals(s);
+    return fixture.echo(s).equals(s);
   }
 
   @Example
   void testHello3() {
-	assertEquals(" ", fixture.echoTwice(""));
+    assertEquals(" ", fixture.echoTwice(""));
   }
 
   @Example
   void testHello4() {
-	assertEquals("world world", fixture.echoTwice("world"));
+    assertEquals("world world", fixture.echoTwice("world"));
   }
 
   @Property
   boolean echoTwiceReturnsItsArgumentTwice(@ForAll final String s) {
-	return fixture.echoTwice(s).equals(s + " " + s);
+    return fixture.echoTwice(s).equals(s + " " + s);
   }
 }
